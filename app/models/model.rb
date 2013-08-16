02b2df82a35d2b2784fd6720e8267210d8a5517e
Austin Roberts <silver2002ws6@yahoo.com>
2013-08-16 16:21:58 -0400

    Models.RB Change

--- app/models/model.rb
+++ app/models/model.rb
@@ -6,7 +6,7 @@ class Model < ActiveRecord::Base
          :rememberable, :trackable, :validatable
 	
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name 
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   # attr_accessible :title, :body
 end 
 
